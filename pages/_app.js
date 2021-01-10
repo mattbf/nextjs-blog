032854bc8a9303459c0cc45f6b50d354b49842e5
Matthew Behan-Fossey <matthewbf8@gmail.com>
2021-01-10 13:46:38 -0700

    Swithced to tailwind css

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/global.css'
+//import "tailwindcss/tailwind.css"; -> other method for using tailwind
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
